ada32cd87b68e1545b50a02237dc8113f76a014c
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-24 00:26:41 +0100

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,30 @@
 var gulp = require('gulp'),
-  nodemon = require('gulp-nodemon'),
   livereload = require('gulp-livereload'),
   less = require('gulp-less'),
-  es6transpiler = require('gulp-es6-transpiler'),
-  cached = require('gulp-cached'),
   http = require('http'),
   st = require('st');
+var ghpages = require('gulp-gh-pages');
 
 gulp.task('less', function () {
-  gulp.src('./sites/**/*.less')
+  gulp.src('./src/sites/**/*.less')
     .pipe(less())
-    .pipe(gulp.dest('./sites/'))
+    .pipe(gulp.dest('./src/sites/'))
     .pipe(livereload());
 });
 
 gulp.task('server', function(done) {
   http.createServer(
-    st({ path: __dirname, index: 'index.html', cache: false })
+    st({ path: __dirname + '/src', index: 'index.html', cache: false })
   ).listen(8080, done);
 });
 
-gulp.task('watch', ['server'], function() {
+gulp.task('default', ['server'], function() {
   livereload.listen();
-  gulp.watch('./sites/**/*.less', ['less']);
-  gulp.watch('src/**/*.js', ['es6']);
+  gulp.watch('./src/sites/**/*.less', ['less']);
 });
 
-gulp.task('es6', function() {
-  return gulp.src('src/**/*.js')
-        .pipe(cached('es6'))
-        .pipe(es6transpiler())
-        .pipe(gulp.dest('dist'));
+// Push to gh-pages
+gulp.task('deploy', function () {
+	return gulp.src('./src/')
+		.pipe(ghpages());
 });
-
-gulp.task('develop', function () {
-  livereload.listen();
-  nodemon({
-    script: 'bin/www',
-    ext: 'js jade',
-    ignore: ['public/*', 'src/*']
-  }).on('restart', function () {
-    setTimeout(function () {
-      //livereload.changed();
-    }, 1500);
-  });
-});
-
-gulp.task('default', [
-  'less',
-  'es6',
-  'develop',
-  'watch'
-]);
